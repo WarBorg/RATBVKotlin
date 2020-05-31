cbc3c34d635177b940fe981fafb047ef18e5a551
Sergiu Cosmin <warborg@gmail.com>
2020-05-31 18:38:35 +0300

    Cleaned up code and names Removed bottom nav bar from bus stations as it is not needed

--- app/src/main/java/com/example/ratbvkotlin/ui/buslines/BusLinesActivity.kt
+++ app/src/main/java/com/example/ratbvkotlin/ui/buslines/BusLinesActivity.kt
@@ -19,8 +19,7 @@ class BusLinesActivity : AppCompatActivity() {
         val navView: BottomNavigationView = findViewById(R.id.bus_line_list_nav_view)
         val navController = findNavController(R.id.bus_line_list_nav_host_fragment)
 
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+        // Passing each menu ID as a set of Ids because each menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.bus_dest,
