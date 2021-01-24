205cc97396263845ec1777a6929a58913beeb65a
Sergiu Cosmin <warborg@gmail.com>
2021-01-24 05:29:46 +0200

    Added main activity and new nav graph

---
+++ app/src/main/java/com/example/ratbvkotlin/ui/BusMainActivity.kt
@@ -0,0 +1,7 @@
+package com.example.ratbvkotlin.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import com.example.ratbvkotlin.R
+
+// Define activity class and link it with layout file
+class BusMainActivity : AppCompatActivity(R.layout.activity_bus_main)
